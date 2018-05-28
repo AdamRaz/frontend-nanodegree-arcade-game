674285ddbb4c11ef964d7fb982d5c7075ae74a20
Adam Razzak <a.abdel@hotmail.co.uk>
2018-05-28 23:58:14 +0100

    edits from udacity code review: using this, prevent scrolling & reordering

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/*
+* Enemy class
+*/
+
 // Enemies our player must avoid
 let Enemy = function() {
     // The image/sprite for our enemies, this uses
@@ -26,6 +30,9 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/*
+* Player class
+*/
 
 let Player = function() {
     // The image/sprite for our player, this uses
@@ -46,17 +53,11 @@ Player.prototype.update = function(dt) {
     if (this.y < -15 ) {
         //AR - this y coordinate corresponds to the top row of the play grid
        // AR - value of this.y here chosen to reset player to beginning when player reaches water zone (goal), we set a 500ms timeout before resetting so player can see themselves enter the water area before they teleport back to the start
-       if (playerStart === 1) {
-            setTimeout(this.reachGoal, 500);
-            playerStart = 0;
-       }
-       // AR - wrapping the timeout inside an if block here prevents the inner reachGoal function from being called multiple times in a row while the player position has yet to be reset
-       // AR - the function here must be a reference, without () else will be executed instantly
+            this.reachGoal();
     }
 
     this.checkCollision();
 };
-
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
@@ -79,6 +80,40 @@ Player.prototype.handleInput = function(keyPress) {
     // AR - the AND '&&' conditionals prevent the player from moving off the grid
 };
 
+Player.prototype.resetMainPlayer = function () {
+    this.x = 200;
+    this.y = 400;
+}
+
+Player.prototype.checkCollision = function () {
+    for (let i = 0; i < allEnemies.length; i++) {
+        if (Math.abs(this.y - allEnemies[i].y) < 20) {
+            if (Math.abs(this.x - allEnemies[i].x) < 60) {
+                this.bugCollision();
+            }
+        }
+        // AR - collision detection here, with a vertical/horizontal range of collision so a rectangular 'hit box' is formed around each enemy bug
+    }
+}
+
+Player.prototype.reachGoal = function () {
+    // AR - wrapping the text update inside an if block here prevents the inner code from being called multiple times in a row while the player position has yet to be reset
+        goalScore++;
+        goalMessage.textContent=`Achieved happiness ${goalScore} time(s)!`;
+    this.resetMainPlayer();
+}
+
+Player.prototype.bugCollision = function () {
+    // AR - wrapping the text update inside an if block here prevents the inner code from being called multiple times in a row while the player position has yet to be reset
+        bugScore++;
+        collisionMessage.textContent=`The bugs got you ${bugScore} time(s)!`;
+
+    this.resetMainPlayer();
+}
+
+/*
+* Object instances & general variables
+*/
 
 // Now instantiate your objects.
 let enemy1 = new Enemy();
@@ -94,61 +129,21 @@ enemy3.speed = 110;
 let allEnemies = [enemy1, enemy2, enemy3];
 // Place the player object in a variable called player
 let player = new Player();
-let playerStart = 1;
 let goalScore = 0;
 let bugScore = 0;
-let textUpdate = 1;
 let goalMessage = document.querySelector('.goal-message');
 let collisionMessage = document.querySelector('.collision-message');
 
-Player.prototype.resetMainPlayer = function () {
-    this.x = 200;
-    this.y = 400;
-    textUpdate = 1;
-    playerStart = 1
-}
-
-Player.prototype.checkCollision = function () {
-    allEnemies.forEach(function(enemy) {
-        if (Math.abs(player.y - enemy.y) < 20) {
-            if (Math.abs(player.x - enemy.x) < 60) {
-                player.bugCollision();
-            }
-        }
-        // AR - collision detection here, with a vertical/horizontal range of collision so a rectangular 'hit box' is formed around each enemy bug
-    });
-}
-
-Player.prototype.reachGoal = function () {
-    // AR - wrapping the text update inside an if block here prevents the inner code from being called multiple times in a row while the player position has yet to be reset
-    if (textUpdate === 1) {
-        textUpdate = 0;
-        goalScore++;
-        goalMessage.textContent=`Achieved happiness ${goalScore} time(s)!`;
-    }
-    player.resetMainPlayer();
-}
-
-Player.prototype.bugCollision = function () {
-    // AR - wrapping the text update inside an if block here prevents the inner code from being called multiple times in a row while the player position has yet to be reset
-    if (textUpdate === 1) {
-        textUpdate = 0;
-        bugScore++;
-        collisionMessage.textContent=`The bugs got you ${bugScore} time(s)!`;
-    }
-
-    player.resetMainPlayer();
-}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
+    e.preventDefault(); // prevents page view from scrolling from cursor key input
     player.handleInput(allowedKeys[e.keyCode]);
 });
